0426200f149bb604571d9726748b04e0c1f43c4c
jonathan <jonathanhinestroza@gmail.com>
2020-11-12 12:29:58 -0500

    enable admin django

--- perfiles_api/admin.py
+++ perfiles_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from perfiles_api import  models
+
+admin.site.register(models.UserProfile)
 # Register your models here.
--- perfiles_api/models.py
+++ perfiles_api/models.py
@@ -13,7 +13,7 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('El usuario debe tener un correo')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name, )
 
         user.set_password(password)
         user.save(using=self._db)
