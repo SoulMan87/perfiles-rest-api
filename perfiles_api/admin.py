55360675031eae20981315641a29e08335c4afa3
jonathan <jonathanhinestroza@gmail.com>
2020-11-12 16:00:20 -0500

    routes, url, rest

--- perfiles_api/admin.py
+++ perfiles_api/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 from perfiles_api import  models
 
 admin.site.register(models.UserProfile)
-# Register your models here.
+
--- perfiles_api/views.py
+++ perfiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test Api View"""
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django view',
+            'Is mapped manually to URLs',
+        ]
+        return Response({'message':'Hello', 'an_apiview': an_apiview})
+
+
+
+
+
